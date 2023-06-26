bb5801c9b8163254a20969714c9621a30220d605
Harshal Kamdi <118383371+harshal1714@users.noreply.github.com>
2023-06-27 00:16:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+// Get the reference to the browser-info element
+const browserInfoElement = document.getElementById('browser-info');
+
+// Get the browser name and version using the navigator object
+const browserName = navigator.userAgent;
+const browserVersion = navigator.appVersion;
+
+// Create the message to be displayed
+const message = "You are using " + browserName + " version " + browserVersion;
+
+// Set the message as the content of the browser-info element
+browserInfoElement.textContent = message;
